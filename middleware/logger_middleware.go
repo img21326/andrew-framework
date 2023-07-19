b6fd332e9cafe2cf7c1491d21d371c8f15f3e606
AndrewLiao <andrew@weibyapps.com>
2023-07-19 12:31:19 +0800

    remove loggger to log

--- middleware/logger_middleware.go
+++ middleware/logger_middleware.go
@@ -1,8 +1,6 @@
 package middleware
 
 import (
-	"time"
-
 	"github.com/img21326/andrew_framework/helper"
 
 	"github.com/gin-gonic/gin"
@@ -11,31 +9,12 @@ import (
 
 func WithLoggerMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		startTime := time.Now()
 		uuid := uuid.New().String()
 		logger := helper.NewLogger(uuid)
 
 		ctx.Set("logger", logger)
 		ctx.Set("uuid", uuid)
-		body, err := ctx.GetRawData()
-		if err != nil {
-			logger.Error(ctx, "get raw data error: %v", err)
-		}
-		logger.Info(ctx, "request params: %+v, request data: %+v", ctx.Request.URL.Query(), string(body))
 
 		ctx.Next()
-		endTime := time.Now()
-		latencyTime := endTime.Sub(startTime)
-		reqMethod := ctx.Request.Method
-		reqUri := ctx.Request.RequestURI
-		statusCode := ctx.Writer.Status()
-		clientIP := ctx.ClientIP()
-		logger.Info(ctx, "| %3d | %13v | %15s | %s | %s |",
-			statusCode,
-			latencyTime,
-			clientIP,
-			reqMethod,
-			reqUri,
-		)
 	}
 }
