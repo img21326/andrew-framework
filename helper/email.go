599146072d62739e9ad70a93ac18f0e0d2a0419f
AndrewLiao <andrew@weibyapps.com>
2023-08-11 15:18:34 +0800

    add email helper

---
+++ helper/email.go
@@ -0,0 +1,63 @@
+package helper
+
+import (
+	"fmt"
+	"net/smtp"
+
+	"github.com/spf13/viper"
+)
+
+var emailInstance *EmailHelper
+
+type EmailHelper struct {
+	host string
+	port int
+	user string
+	pass string
+}
+
+func newEmailHelper() *EmailHelper {
+	host := viper.GetViper().GetString("EMAIL_HOST")
+	port := viper.GetViper().GetInt("EMAIL_PORT")
+	user := viper.GetViper().GetString("EMAIL_USER")
+	pass := viper.GetViper().GetString("EMAIL_PASS")
+
+	if host == "" || port == 0 || user == "" || pass == "" {
+		panic("Email config error")
+	}
+
+	return &EmailHelper{
+		host: host,
+		port: port,
+		user: user,
+		pass: pass,
+	}
+}
+
+func GetEmailHelper() *EmailHelper {
+	if emailInstance == nil {
+		emailInstance = newEmailHelper()
+	}
+	return emailInstance
+}
+
+type EmailSendOption struct {
+	From    string
+	To      []string
+	Subject string
+	Body    string
+}
+
+func (e *EmailHelper) SendEmail(option EmailSendOption) error {
+	var from string
+	if option.From == "" {
+		from = e.user
+	}
+
+	message := []byte(option.Subject + option.Body)
+
+	auth := smtp.PlainAuth("", e.user, e.pass, e.host)
+
+	err := smtp.SendMail(fmt.Sprintf("%s:%s", e.host, e.port), auth, from, option.To, message)
+	return err
+}
